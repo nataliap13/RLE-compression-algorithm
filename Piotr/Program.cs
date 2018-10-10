c2804feac8177fd0f3c2b2abdc91025e48b9f8f9
Slasheruus <piotr.wolynski.kontakt@gmail>
2018-10-10 22:08:17 +0200

    second commit

---
+++ Piotr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader.read();
+            Reader.dict.ToList().ForEach(x => Console.WriteLine(x.Key + " Value: " + x.Value));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Piotr/Reader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    static class Reader
+    {
+        static string filePath = @"C:\Users\Piotr\source\repos\dziala\input.txt";
+        static public List<string> lines = File.ReadAllLines(filePath).ToList();
+        static public Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        static public void read()
+        {
+            // every line we read separately
+            foreach (string line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {   // if there isn't the char in the dictionary we add key
+                    if (!dict.ContainsKey(line[i]))
+                        dict.Add(line[i], 1);
+                    // if there is the char in the dictionary we just increase the value
+                    else
+                        dict[line[i]] += 1;
+                }
+            }
+        }
+    }
+}
+
+
