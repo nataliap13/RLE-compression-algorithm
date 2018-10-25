8a022c2ebb04a5f001249f1387afc607701e1263
nataliap13 <nataliapop@op.pl>
2018-10-25 13:30:42 +0200

    works
    but not good enough

--- TIIK proj/FileLoaderWindow.xaml.cs
+++ TIIK proj/FileLoaderWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace TIIK_proj
     /// </summary>
     public partial class FileLoaderWindow : Window
     {
+        List<CharCountObject> MainList;
         public FileLoaderWindow()
         {
             InitializeComponent();
@@ -50,20 +51,65 @@ namespace TIIK_proj
             var textToAnalyze = TextBoxFileContent.Text;
             var TxtLength = textToAnalyze.Length;
             LabelCharsCount.Content = textToAnalyze.Length;
-            Dictionary<char, int> dict = new Dictionary<char, int>();
+            Dictionary<char, int> dictCharsAndFrequency = new Dictionary<char, int>();
             foreach (var character in textToAnalyze)
             {                
-                if (!dict.ContainsKey(character))// if there isn't the char in the dictionary we add key
-                { dict.Add(character, 1); }
+                if (!dictCharsAndFrequency.ContainsKey(character))// if there isn't the char in the dictionary we add key
+                { dictCharsAndFrequency.Add(character, 1); }
                 else // if there is the char in the dictionary we just increase the value
-                { dict[character] += 1; }
+                { dictCharsAndFrequency[character] += 1; }
             }
-            var dictAsCharCountObjectList = new List<CharCountObject>();
-            foreach (var pair in dict)
+
+            //converting dictionary to list to display it in dataGrid
+            var ListFromDictAsCharCountObjects = new List<CharCountObject>();
+            var percentCount = 0.000;
+            foreach (var pair in dictCharsAndFrequency)
             {
-                dictAsCharCountObjectList.Add(new CharCountObject() { Character = pair.Key, Count = pair.Value, Probability = 100.0 * (double)pair.Value/ (double)TxtLength });
+                var prob = 100.0 * (double)pair.Value / (double)TxtLength;
+                prob = Math.Round(prob, 3);
+                if (prob == 0.000)
+                { prob = 0.001; }
+                percentCount += prob;
+                ListFromDictAsCharCountObjects.Add(new CharCountObject() { Character = pair.Key, Count = pair.Value, Probability = prob });
             }
-            dataGridCharsCount.ItemsSource = dictAsCharCountObjectList;
+
+            if(percentCount!=100.0)
+            { LabelPercentWarning.Content = "Wynik != 100%"; LabelPercentWarning.Foreground = Brushes.Red; }
+            else
+            { LabelPercentWarning.Content = "100%"; LabelPercentWarning.Foreground = Brushes.Black; }
+
+            var sortedList = ListFromDictAsCharCountObjects.OrderByDescending(x => x.Probability).ToList();//sort
+            MainList = sortedList;
+            dataGridCharsCount.ItemsSource = sortedList;
+            LabelPercentCount.Content = (percentCount == 100.000 ? 100.000 : percentCount);
+        }
+
+        private void ButtonPercentReduction_Click(object sender, RoutedEventArgs e)
+        {
+            var workList = MainList.OrderBy(x => x.Probability).ToList();//sort
+            foreach(var elem in workList)
+            {
+                var percent = 0.0;
+                foreach (var el in workList)
+                {
+                    percent += el.Probability;
+                }
+                percent -= 100.0;
+                if (percent == 0.0)
+                { break; }
+                LabelPercentWarning.Content = percent;
+                //System.Threading.Thread.Sleep(3000);
+                if (percent != 0.0)
+                {
+                    elem.Probability -= Math.Sign(percent) * 0.001;
+                }
+                else break;
+            }
+
+            LabelPercentWarning.Content = "100%"; LabelPercentWarning.Foreground = Brushes.Black;
+            MainList = workList.OrderByDescending(x => x.Probability).ToList();//sort
+            dataGridCharsCount.ItemsSource = MainList;
+            LabelPercentCount.Content = (100.000);
         }
     }
 }
