43a7a8821ef45cdaa43ea1b8ae570a4af8456010
nataliap13 <nataliapop@op.pl>
2019-04-18 16:58:36 +0200

    Update RLE.cs

--- TIIK proj/RLE.cs
+++ TIIK proj/RLE.cs
@@ -43,41 +43,9 @@ namespace TIIK_proj
             { Encoded = Encoded + toappend; }
             else
             { Encoded = Encoded + toappend + toappend + (char)count; }
-            /*if (count == -1)
-            { EncodedList.Add(toappend); }
-            else
-            {
-                EncodedList.Add(toappend);
-                EncodedList.Add(toappend);
-                EncodedList.Add((char)count);
-            }*/
         }
-        /*
-        public static string Decode(string toDecode)
-        {
-            //int toDecodeLen = toDecode.Length;
-            string decoded = string.Empty;
-            //Console.WriteLine("toDecodeLen " + toDecodeLen);
-            while (toDecode.Length > 1)
-            {
-                if (toDecode[0] == toDecode[1])
-                {
-                    int count = (int)toDecode[2];
-                    // bo ma byc dopisanych znakow aa3 -> 3 a wiec sumarycznie 3+2=5, dlatego petla sie obraca count+2
-                    decoded += new String(toDecode[0], count + 2);
-                    toDecode = toDecode.Remove(0, 3);
-                }
-                else
-                {
-                    decoded += toDecode[0];
-                    toDecode = toDecode.Remove(0, 1);
-                }
-            }
-            if (toDecode.Length == 1)
-            { decoded += toDecode[0]; }
-            return decoded;
-        }*/
-        ///*
+        
+        //decoding
         public static string Decode(string TextToDecode)
         {
             List<char> toDecode = TextToDecode.ToList();
@@ -102,7 +70,7 @@ namespace TIIK_proj
             if (toDecode.Count == 1)
             { decoded += toDecode[0]; }
             return decoded;
-        }//*/
+        }
     }
 }
 
